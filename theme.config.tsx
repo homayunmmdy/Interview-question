6864f1777c1050f79a702916f9b1d0c6367344d3
Homayoun <homayoun763@gmail.com>
2024-03-01 22:56:31 +0330

    fist page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span> Interview Questions</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/homayunmmdy',
   },
   chat: {
     link: 'https://discord.com',
